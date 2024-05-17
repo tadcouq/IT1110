9533cf54f65d82efe8067283e8fc8a56c33b5b36
Nguyen Dat <88364090+tadcouq@users.noreply.github.com>
2024-05-17 12:57:28 +0700

    Update Bai3_3-20231570_DatNQ.c

--- Bai3/Bai3_3-20231570_DatNQ.c
+++ Bai3/Bai3_3-20231570_DatNQ.c
@@ -1,26 +1,10 @@
 // 2024/04/05 20:01:27 GMT+7 (c) https://tdcq.me/
-#include <stdio.h>
-#include <math.h>
+
+#include<stdio.h>
 
 int main(){
-    int x,y;
-    float A,B,C;
-
-    // Nhập giá trị cho x, y
-    printf("Nhap gia tri cho x: ");
-    scanf("%d", &x);
-    printf("Nhap gia tri cho y: ");
-    scanf("%d", &y);
-
-    // Tính A, B, C
-    A=x^2+y^2;
-    B=(x+y)^2;
-    C=(x-y)^2;
-
-    // In kết quả
-    printf("A = %f\n", A);
-    printf("B = %f\n", B);
-    printf("C = %f\n", C);
-
+    float x, y;
+    scanf("%f%f", &x, &y);
+    printf("%f\n%f\n%f", x*x + y*y, (x+y)*(x+y), (x-y)*(x-y));
     return 0;
 }
