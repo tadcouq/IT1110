ec07e534f5176c724e1f19f67ec4a30ec56fe83f
Nguyen Dat <88364090+tadcouq@users.noreply.github.com>
2024-05-17 13:39:05 +0700

    Update Bai3_3-20231570_DatNQ.c

--- Bai3/Bai3_3-20231570_DatNQ.c
+++ Bai3/Bai3_3-20231570_DatNQ.c
@@ -1,10 +1,11 @@
 // 2024/04/05 20:01:27 GMT+7 (c) https://tdcq.me/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(){
-    float x, y;
-    scanf("%f%f", &x, &y);
-    printf("%f\n%f\n%f", x*x + y*y, (x+y)*(x+y), (x-y)*(x-y));
+    double x, y;
+    scanf("%lf%lf", &x, &y);
+    printf("%lf\n%lf\n%lf", pow(x,2) + pow(y,2), pow(x+y,2), pow(x-y,2));
     return 0;
 }
