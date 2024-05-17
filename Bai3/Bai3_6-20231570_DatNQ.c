44357cbae6ab15099e1eae18dec46408701bc414
Nguyen Dat <88364090+tadcouq@users.noreply.github.com>
2024-05-17 13:09:05 +0700

    Update Bai3_6-20231570_DatNQ.c

--- Bai3/Bai3_6-20231570_DatNQ.c
+++ Bai3/Bai3_6-20231570_DatNQ.c
@@ -1,22 +1,21 @@
 // 2024/04/05 20:22:07 GMT+7 (c) https://tdcq.me/
-#include <stdio.h>
+#include<stdio.h>
 #include <math.h>
-
-// Biểu thức khá dài, phân thành void function riêng cho dễ nhìn
-void cal(float x, float y) {
-    float F = y^6 + x*y^5 + x^2*y^4 + x^3*y^3 + x^4*y^2 + x^5*y + x^6;
-    printf("Gia tri cua F la: %.2f\n", F);
+double power(double x, int x1, double y, int y1){
+    double f;
+    f=pow(x, x1)*pow(y, y1);
+    return f;
 }
-
-int main() {
-    float x, y;
-
-    printf("Nhap x: ");
-    scanf("%f", &x);
-    printf("Nhap y: ");
-    scanf("%f", &y);
-
-    cal(x, y);
-
-    return 0;
+int main(){
+    double x, y, F;
+    scanf("%lf", &x);
+    scanf("%lf", &y);
+    F=power(x, 0, y, 6)+
+    power(x, 1, y, 5)+
+    power(x, 2, y, 4)+
+    power(x, 3, y, 3)+
+    power(x, 4, y, 2)+
+    power(x, 5, y, 1)+
+    power(x, 6, y, 0);
+    printf("%.4lf", F);
 }
