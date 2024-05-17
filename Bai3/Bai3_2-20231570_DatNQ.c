cfbd723288f2db89dd597b4555f419c356dcbfda
Nguyen Dat <88364090+tadcouq@users.noreply.github.com>
2024-05-17 13:20:08 +0700

    Update Bai3_2-20231570_DatNQ.c

--- Bai3/Bai3_2-20231570_DatNQ.c
+++ Bai3/Bai3_2-20231570_DatNQ.c
@@ -1,27 +1,10 @@
 // 2024/04/05 19:55:39 GMT+7 (c) https://tdcq.me/
-#include <stdio.h>
-#define PI 3.14159
+#include<stdio.h>
+#define pi 3.14159
 
-// Kiểm tra tính hợp lệ của input và tính chu vi diện tích hình tròn
-void checkCircle(float r) {
-    if (r > 0) {
-        // Tính chu vi và diện tích hình tròn
-        float chuvi = 2 * PI * r;
-        float dientich = PI * r * r;
-        printf("Chu vi cua hinh tron la: %.2f\n", chuvi);
-        printf("Dien tich cua hinh tron la: %.2f\n", dientich);
-    } else {
-        printf("Ban kinh hinh tron phai lon hon 0.\n");
-    }
-}
-
-int main() {
+int main(){
     float r;
-
-    printf("Nhap ban kinh hinh tron: ");
     scanf("%f", &r);
-
-    checkCircle(r);
-
+    if (r > 0) printf("%f\n%f", 2*r*pi, r*r*pi); else printf("ERROR");
     return 0;
-}
+}s
